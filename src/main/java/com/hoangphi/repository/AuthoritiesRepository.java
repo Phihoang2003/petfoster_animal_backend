375d6505cce4a99e2ce6b28d74a72d2fcf6ded4d
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-07-26 20:36:24 +0700

    modified AuthoritiesRepository

--- src/main/java/com/hoangphi/repository/AuthoritiesRepository.java
+++ src/main/java/com/hoangphi/repository/AuthoritiesRepository.java
@@ -3,9 +3,10 @@ package com.hoangphi.repository;
 import com.hoangphi.entity.Authorities;
 import com.hoangphi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface AuthoritiesRepository extends JpaRepository<Authorities,Integer> {
     List<Authorities> findByUser(User user);
 }

bbb1076a662dabcab0b9d34f20213fcb1e7115d9
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-07-27 01:11:03 +0700

    bug for query to get data

--- src/main/java/com/hoangphi/entity/Posts.java
+++
@@ -1,4 +0,0 @@
-package com.hoangphi.entity;
-
-public class Posts {
-}
--- src/main/java/com/hoangphi/repository/UserRepository.java
+++ src/main/java/com/hoangphi/repository/UserRepository.java
@@ -7,17 +7,18 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,String> {
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
     Boolean existsByPhone(String phone);
     User findByUuid(String uuid);
-    @Query("select u from Users u where u.email = :email")
-    public Optional<User> findByEmail(@Param("email") String email);
-    @Query("select u from Users u where u.username=:username")
-    public Optional<User> findByUsername(@Param("username") String username);
-    @Query(nativeQuery = true,value="select * from Users where Users.token = :token")
-    public User findByToken(@Param("token") String token);
+//    @Query("select u from Users u where u.email = :email")
+//    public Optional<User> findByEmail(@Param("email") String email);
+//    @Query("select u from Users u where u.username=:username")
+//    public Optional<User> findByUsername(@Param("username") String username);
+//    @Query(nativeQuery = true,value="select * from Users where Users.token = :token")
+//    public User findByToken(@Param("token") String token);
 
 }

38432c7e090a7b0f60a679a322bd1fa71da39e84
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-07-26 18:29:26 +0700

    add AuthResponse and Role,Api Response

---
+++ src/main/java/com/hoangphi/entity/Role.java
@@ -0,0 +1,28 @@
+package com.hoangphi.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Nationalized
+    private String role;
+    @Nationalized
+    private String roleDesc;
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Authorities> authorities;
+}

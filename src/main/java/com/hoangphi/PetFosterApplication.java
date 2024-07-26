b5df002fab95f2b129d5c521bef67111d9fe3e63
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-07-26 14:52:44 +0700

    initial project and set up folder

---
+++ src/main/java/com/hoangphi/PetFosterApplication.java
@@ -0,0 +1,11 @@
+package com.hoangphi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFosterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PetFosterApplication.class, args);
+    }
+}
---
+++ src/main/java/com/hoangphi/service.java
@@ -0,0 +1,4 @@
+package com.hoangphi;
+
+public class service {
+}

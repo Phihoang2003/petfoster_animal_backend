ffca5a402d61fe9b180ef1c5bf438f07b9582ee8
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-07-26 18:31:12 +0700

    configuration server

--- src/main/java/com/hoangphi/entity/Authorities.java
+++ src/main/java/com/hoangphi/entity/Authorities.java
@@ -1,4 +1,24 @@
 package com.hoangphi.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Authorities {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
 }

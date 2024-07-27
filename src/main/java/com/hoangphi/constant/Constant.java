424bc3b1d2b33d7fe4030fb12cdb2f9f295bcf1b
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-07-27 14:14:42 +0700

    add constant secret key

--- src/main/java/com/hoangphi/constant/Constant.java
+++ src/main/java/com/hoangphi/constant/Constant.java
@@ -2,4 +2,5 @@ package com.hoangphi.constant;
 
 public class Constant {
     public static final String BASE_URL="http://localhost:8019/login/";
+    public static final String SECRET_KEY = "dfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhf";
 }

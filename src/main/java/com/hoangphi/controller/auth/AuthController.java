e7845d5f49cafde6ca755ea0ecc50df1aa86c8a9
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-07-27 14:10:47 +0700

    create controller for login user

--- src/main/java/com/hoangphi/controller/auth/AuthController.java
+++ src/main/java/com/hoangphi/controller/auth/AuthController.java
@@ -1,5 +1,6 @@
 package com.hoangphi.controller.auth;
 
+import com.hoangphi.request.LoginRequest;
 import com.hoangphi.request.RegisterRequest;
 import com.hoangphi.response.AuthResponse;
 import com.hoangphi.service.AuthService;
@@ -16,7 +17,6 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/")
-
 public class AuthController {
     @Autowired
     HttpServletRequest httpServletRequest;
@@ -24,8 +24,12 @@ public class AuthController {
      AuthService authService;
     @PostMapping("register")
     public ResponseEntity<AuthResponse> register(HttpServletRequest httpServletRequest, @Valid @RequestBody RegisterRequest registerRequest){
-        System.out.println("registerRequest: "+"Hejej" );
         httpServletRequest.setAttribute("username", registerRequest.getUsername());
         return ResponseEntity.ok(authService.register(httpServletRequest, registerRequest));
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
     }
 }

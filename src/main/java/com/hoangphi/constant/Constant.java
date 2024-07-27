eb8513392cdfda6b27d67f4234e920074abfc6f9
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-07-27 14:30:58 +0700

    add findByToken in user repository

--- src/main/java/com/hoangphi/constant/Constant.java
+++ src/main/java/com/hoangphi/constant/Constant.java
@@ -3,4 +3,5 @@ package com.hoangphi.constant;
 public class Constant {
     public static final String BASE_URL="http://localhost:8019/login/";
     public static final String SECRET_KEY = "dfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhf";
+    public static final Integer TOKEN_EXPIRE_LIMIT = 5 * 60 * 1000;
 }

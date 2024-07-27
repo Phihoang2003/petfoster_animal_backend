1705e72784fb1aee0b8b9bed0ebfdd1858467f21
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-07-27 15:29:49 +0700

    create full properties for Addresses

--- src/main/java/com/hoangphi/entity/Addresses.java
+++ src/main/java/com/hoangphi/entity/Addresses.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Nationalized;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -17,6 +19,21 @@ public class Addresses {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String recipient;
+    private String phone;
+    @Nationalized
+    private String province;
+    @Nationalized
+    private String district;
+    @Nationalized
+    private String ward;
+    @Nationalized
+    private String address;
+    @JoinColumn(name = "is_default")
+    private Boolean isDefault;
+    @JoinColumn(name = "create_at")
+    private Date createAt;
+
     @ManyToOne
     @JsonIgnore
     @JoinColumn(name = "user_id")

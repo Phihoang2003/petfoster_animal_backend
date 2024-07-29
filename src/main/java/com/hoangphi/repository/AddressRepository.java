495ed9d3ee6d15278cba4003043f664a607c37e7
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-07-29 14:53:38 +0700

    create addresses repository for findByUser and findByIsDefaultUser

---
+++ src/main/java/com/hoangphi/repository/AddressRepository.java
@@ -0,0 +1,16 @@
+package com.hoangphi.repository;
+
+import com.hoangphi.entity.Addresses;
+import com.hoangphi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AddressRepository extends JpaRepository<Addresses, Integer>{
+    @Query("select a from Addresses a where a.isDefault=true and a.user.username=:username")
+    Addresses findByIsDefaultWithUser(@Param("username") String username);
+
+    List<Addresses> findByUser(User user);
+}

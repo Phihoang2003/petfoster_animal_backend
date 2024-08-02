dade3735a32a98abdfe7038c3d9bc189cf40ae70
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-02 16:47:50 +0700

    create service postOfUser and buildHomePageResponse

--- src/main/java/com/hoangphi/service/posts/PostService.java
+++ src/main/java/com/hoangphi/service/posts/PostService.java
@@ -1,12 +1,20 @@
 package com.hoangphi.service.posts;
 
+import com.hoangphi.entity.social.Posts;
 import com.hoangphi.request.posts.PostRequest;
 import com.hoangphi.request.posts.PostUpdateRequest;
 import com.hoangphi.response.ApiResponse;
+import com.hoangphi.response.posts.PostReponse;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PostService {
     ApiResponse create(PostRequest data, String token);
     ApiResponse update(PostUpdateRequest data, String id, String token);
     ApiResponse delete(String uuid, String token);
     ApiResponse likePost(String uuid, String token);
+    List<PostReponse> buildPostHomePageResponses(List<Posts> posts);
+
+    ApiResponse postsOfUser(String username, Optional<Integer> page, Optional<String> type);
 }

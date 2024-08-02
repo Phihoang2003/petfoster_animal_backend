0c6f5f9a455a41ae0a60cd44703c166409ed3518
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-02 16:49:00 +0700

    add controller for postOfUser

--- src/main/java/com/hoangphi/controller/posts/PostController.java
+++ src/main/java/com/hoangphi/controller/posts/PostController.java
@@ -1,13 +1,23 @@
 package com.hoangphi.controller.posts;
 
+import com.hoangphi.response.ApiResponse;
 import com.hoangphi.service.posts.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
 public class PostController {
     private final PostService postService;
+
+    @GetMapping("{username}")
+    public ResponseEntity<ApiResponse> postOfUser(@PathVariable("username") String username,
+                                                  @RequestParam("type") Optional<String> type,
+                                                  @RequestParam("page") Optional<Integer> page){
+        return ResponseEntity.ok(postService.postsOfUser(username, page, type));
+    }
 }

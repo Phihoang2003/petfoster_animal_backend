c0f831a1bbc691baef48535fa74adc0fcc4be6ec
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-03 13:05:46 +0700

    create search posts with @Query repository

--- src/main/java/com/hoangphi/repository/PostRepository.java
+++ src/main/java/com/hoangphi/repository/PostRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Posts, Integer>{
@@ -17,5 +18,8 @@ public interface PostRepository extends JpaRepository<Posts, Integer>{
 
     @Query(value = "select p from Posts p where p.user.username = :username order by p.createAt desc")
     List<Posts> postOfUser(String username);
+
+    @Query("select p from Posts p where (:search IS NULL OR p.title LIKE %:search% OR p.id LIKE %:search% OR p.user.username LIKE %:search%) order by p.createAt desc")
+    List<Posts> posts(@Param("search") Optional<String> search);
 
 }

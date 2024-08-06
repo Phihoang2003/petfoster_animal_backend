650ae2e59bd9ef449de83ab04fb3e5bd19a140d6
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-06 10:03:51 +0700

    create utils to return url for image

---
+++ src/main/java/com/hoangphi/utils/PortUtils.java
@@ -0,0 +1,43 @@
+package com.hoangphi.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+
+@Component
+public class PortUtils {
+    @Autowired
+    private Environment environment;
+
+    private String port;
+
+    public String getPort(){
+        if(port==null)
+            port=environment.getProperty("local.server.port");
+        return port;
+    }
+    public String getServerUrlPrefix(){
+        return "http://"+ InetAddress.getLoopbackAddress().getHostName()+":"+getPort();
+    }
+    public String getUrlImage(String name)
+    {
+        if(Validate.isUrl(name)){
+            return name;
+        }
+        return getServerUrlPrefix()+"/images/"+name;
+    }
+    public String getUrlImage(String name, String pathName) {
+        // check if name is url return name
+        if (Validate.isUrl(name)) {
+            return name;
+        }
+        return getServerUrlPrefix() + "/images/" + pathName + "/" + name;
+    }
+
+
+
+
+
+}

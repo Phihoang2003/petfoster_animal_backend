ef26ba4811e2078c2fa797607dc46eb670b56bff
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-07 17:02:50 +0700

    create findByPost repository using nativeQuery successfully

--- src/main/java/com/hoangphi/repository/CommentRepository.java
+++ src/main/java/com/hoangphi/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.hoangphi.repository;
 
 import com.hoangphi.entity.social.Comments;
+import com.hoangphi.entity.social.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +13,7 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comments, Integer>{
     @Query(nativeQuery = true,value="select * from comments where comments.post_id=:postId and comments.reply_to =:commentId")
     List<Comments> findByReply(@Param("postId") Integer postId, @Param("commentId") Integer commentId);
+
+    @Query("select c from Comments c where c.replyTo is null and c.post=:post order by c.createAt desc")
+    List<Comments> findByPost(@Param("post") Posts post);
 }

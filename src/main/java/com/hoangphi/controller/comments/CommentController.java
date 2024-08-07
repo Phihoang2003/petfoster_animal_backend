ae6fcf51a53ca50c64ccee28aa6da320809f9c7f
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-07 17:03:17 +0700

    create find by post controller and service

--- src/main/java/com/hoangphi/controller/comments/CommentController.java
+++ src/main/java/com/hoangphi/controller/comments/CommentController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/comments")
@@ -16,4 +18,11 @@ public class CommentController {
     public ResponseEntity<ApiResponse> likedComment(@PathVariable("comment_id") Integer commentId, @RequestHeader("Authorization") String token){
         return ResponseEntity.ok(commentService.likeComment(commentId, token));
     }
+
+    @GetMapping("{post_id}")
+    public ResponseEntity<ApiResponse> getCommentWithPost(@PathVariable("post_id") String uuid,
+                                                          @RequestParam("page") Optional<Integer> page) {
+        return ResponseEntity.ok(commentService.getCommentWithIdPost(uuid, page));
+    }
+
 }
--- src/main/java/com/hoangphi/service/comments/CommentService.java
+++ src/main/java/com/hoangphi/service/comments/CommentService.java
@@ -3,8 +3,12 @@ package com.hoangphi.service.comments;
 import com.hoangphi.request.comments.CommentPostRequest;
 import com.hoangphi.response.ApiResponse;
 
+import java.util.Optional;
+
 public interface CommentService {
     ApiResponse commentPost(CommentPostRequest commentPostRequest, String token);
     ApiResponse likeComment(Integer commentId, String token);
+
+    ApiResponse getCommentWithIdPost(String uuid, Optional<Integer> page);
 
 }

30ec48b44cc0d12ef37922c29a87573678f041c5
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-11 17:03:34 +0700

    add delete, getProductRepo and createProductRepo

--- src/main/java/com/hoangphi/repository/ProductRepoRepository.java
+++ src/main/java/com/hoangphi/repository/ProductRepoRepository.java
@@ -3,6 +3,8 @@ package com.hoangphi.repository;
 import com.hoangphi.entity.Product;
 import com.hoangphi.entity.ProductRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,7 @@ import java.util.List;
 @Repository
 public interface ProductRepoRepository extends JpaRepository<ProductRepo, Integer>{
     List<ProductRepo> findByProduct(Product product);
+
+    @Query("select rp from ProductRepo rp where rp.isActive=true and rp.product.id=:productId order by rp.size")
+    List<ProductRepo> findByProductSorting(@Param("productId") String productId);
 }

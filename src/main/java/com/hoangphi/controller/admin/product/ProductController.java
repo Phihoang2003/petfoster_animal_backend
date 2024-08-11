d5d792f58dc3a9730c7883cc9a8b6a42eb3d42c9
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-11 11:34:50 +0700

    create controller and repository update product

--- src/main/java/com/hoangphi/controller/admin/product/ProductController.java
+++ src/main/java/com/hoangphi/controller/admin/product/ProductController.java
@@ -1,6 +1,7 @@
 package com.hoangphi.controller.admin.product;
 
 import com.hoangphi.request.CreateProductRequest;
+import com.hoangphi.request.products.ProductRequest;
 import com.hoangphi.response.ApiResponse;
 import com.hoangphi.service.admin.products.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +21,12 @@ public class ProductController {
     public ResponseEntity<ApiResponse> createProduct(@ModelAttribute CreateProductRequest createProductRequest,
                                                      @RequestPart List<MultipartFile> images) {
         return ResponseEntity.ok(productService.createProduct(createProductRequest, images));
+    }
+
+    @PostMapping(value = "/{id}")
+    public ResponseEntity<ApiResponse> updateProduct(@PathVariable("id") String id,
+                                                     @RequestBody ProductRequest productRequest) {
+        return ResponseEntity.ok(productService.updateProduct(id, productRequest));
+
     }
 }
--- src/main/java/com/hoangphi/repository/ProductRepository.java
+++ src/main/java/com/hoangphi/repository/ProductRepository.java
@@ -4,9 +4,11 @@ import com.hoangphi.entity.Product;
 import com.hoangphi.response.ApiResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>{
@@ -16,4 +18,8 @@ public interface ProductRepository extends JpaRepository<Product, Integer>{
 
     @Query("SELECT p FROM Product p")
     public List<Product> findAllNoActive();
+    boolean existsById(String id);
+
+    @Query("select p from Product p where p.id = :id")
+    public Optional<Product> findById(@Param("id") String id);
 }

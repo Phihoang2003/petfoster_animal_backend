a065af697a692d46138fa77af5a43ffad6f700c1
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-11 14:21:29 +0700

    add getProductById and getAllProduct

--- src/main/java/com/hoangphi/controller/admin/product/ProductController.java
+++ src/main/java/com/hoangphi/controller/admin/product/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/admin/product")
@@ -29,4 +30,18 @@ public class ProductController {
         return ResponseEntity.ok(productService.updateProduct(id, productRequest));
 
     }
+    @GetMapping("/info/{id}")
+    public ResponseEntity<ApiResponse> getProductInfo(@PathVariable("id") String id) {
+        return ResponseEntity.ok(productService.getProductInfo(id));
+    }
+    @GetMapping("/get-all")
+    public ResponseEntity<ApiResponse> getAllProduct(@RequestParam("page") Optional<Integer> page) {
+        return ResponseEntity.ok(productService.getAllProduct(page));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse> getProduct(@PathVariable("id") String id) {
+        return ResponseEntity.ok(productService.getProduct(id));
+    }
+
 }
--- src/main/java/com/hoangphi/repository/ProductRepoRepository.java
+++ src/main/java/com/hoangphi/repository/ProductRepoRepository.java
@@ -1,9 +1,13 @@
 package com.hoangphi.repository;
 
+import com.hoangphi.entity.Product;
 import com.hoangphi.entity.ProductRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepoRepository extends JpaRepository<ProductRepo, Integer>{
+    List<ProductRepo> findByProduct(Product product);
 }

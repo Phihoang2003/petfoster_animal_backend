b1c090a937697f6a8c856d40c2fd6f247c4fb1b9
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-13 11:59:21 +0700

    resolve problem with update more images by productId

--- src/main/java/com/hoangphi/controller/admin/images/ImagesController.java
+++ src/main/java/com/hoangphi/controller/admin/images/ImagesController.java
@@ -4,10 +4,10 @@ import com.hoangphi.response.ApiResponse;
 import com.hoangphi.service.admin.images.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +18,12 @@ public class ImagesController {
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse> getImagesByIdProduct(@PathVariable("id") String id) {
         return ResponseEntity.ok(imageService.getImagesByIdProduct(id));
+    }
+
+
+    @PostMapping("/{id}")
+    public ResponseEntity<ApiResponse> addImagesByIdProduct(@PathVariable("id") String id,
+                                                            @RequestPart("images") List<MultipartFile> images) {
+        return ResponseEntity.ok(imageService.addImagesByIdProduct(id, images));
     }
 }
--- src/main/java/com/hoangphi/service/impl/images/ImageServiceImpl.java
+++ src/main/java/com/hoangphi/service/impl/images/ImageServiceImpl.java
@@ -1,23 +1,29 @@
 package com.hoangphi.service.impl.images;
 
 import com.hoangphi.entity.Imgs;
+import com.hoangphi.entity.Product;
 import com.hoangphi.repository.ImagesRepository;
+import com.hoangphi.repository.ProductRepository;
 import com.hoangphi.response.ApiResponse;
 import com.hoangphi.response.images.ImagesResponse;
 import com.hoangphi.service.admin.images.ImageService;
+import com.hoangphi.utils.ImageUtils;
 import com.hoangphi.utils.PortUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
     private final ImagesRepository imagesRepository;
     private final PortUtils portUtils;
+    private final ProductRepository productRepository;
 
     @Override
     public byte[] getImage(String fileName) {
@@ -31,7 +37,7 @@ public class ImageServiceImpl implements ImageService {
 
     @Override
     public ApiResponse getImagesByIdProduct(String id) {
-        if(id.isEmpty()){
+        if (id.isEmpty()) {
             return ApiResponse.builder()
                     .message("Id is empty")
                     .data(null)
@@ -39,8 +45,8 @@ public class ImageServiceImpl implements ImageService {
                     .status(HttpStatus.NOT_FOUND.value())
                     .build();
         }
-        List<Imgs> images=imagesRepository.getImagesByProductId(id);
-        if(images.isEmpty()){
+        List<Imgs> images = imagesRepository.getImagesByProductId(id);
+        if (images.isEmpty()) {
             return ApiResponse.builder()
                     .message("Not found")
                     .data(null)
@@ -48,7 +54,7 @@ public class ImageServiceImpl implements ImageService {
                     .status(HttpStatus.NOT_FOUND.value())
                     .build();
         }
-        List<ImagesResponse> imagesResponses=images.stream().map(item->{
+        List<ImagesResponse> imagesResponses = images.stream().map(item -> {
             return ImagesResponse.builder()
                     .id(item.getId())
                     .name(item.getNameImg())
@@ -68,7 +74,63 @@ public class ImageServiceImpl implements ImageService {
 
     @Override
     public ApiResponse addImagesByIdProduct(String id, List<MultipartFile> images) {
-        return null;
+        final int MAX_IMAGE = 4;
+        if (id.isEmpty() || images.isEmpty()) {
+            return ApiResponse.builder()
+                    .message("Id or images is empty")
+                    .data(null)
+                    .errors(true)
+                    .status(HttpStatus.NOT_FOUND.value())
+                    .build();
+        }
+
+        if (Objects.equals(images.get(0).getOriginalFilename(), "")) {
+            return ApiResponse.builder()
+                    .message("Images or Id invalid ")
+                    .status(400)
+                    .errors(true)
+                    .data(null)
+                    .build();
+        }
+        Product product=productRepository.findById(id).orElse(null);
+        List<Imgs> imageOfProduct=imagesRepository.getImagesByProductId(id);
+        if (product==null){
+            return ApiResponse.builder()
+                    .message("Product not found")
+                    .status(HttpStatus.NOT_FOUND.value())
+                    .errors(true)
+                    .data(null)
+                    .build();
+        }
+        if(images.size()+imageOfProduct.size()>MAX_IMAGE){
+            return ApiResponse.builder()
+                    .message("Limit image for product is 4")
+                    .status(HttpStatus.BAD_REQUEST.value())
+                    .errors(true)
+                    .data(null)
+                    .build();
+        }
+        List<Imgs> newListImgs=images.stream().map(image->{
+            try{
+                File file= ImageUtils.createFileImage();
+                image.transferTo(new File(file.getAbsolutePath()));
+                return Imgs.builder()
+                        .nameImg(file.getName())
+                        .product(product)
+                        .build();
+
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+                return null;
+            }
+        }).toList();
+
+        return ApiResponse.builder()
+                .message("Add images successfuly")
+                .status(HttpStatus.OK.value())
+                .errors(false)
+                .data(imagesRepository.saveAll(newListImgs))
+                .build();
     }
 
     @Override

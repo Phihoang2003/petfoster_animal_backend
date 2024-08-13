ceb0dfc4ea59e37aaff44b47021a516178f27a22
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-13 19:14:49 +0700

    create entity favourite

---
+++ src/main/java/com/hoangphi/entity/Favorite.java
@@ -0,0 +1,29 @@
+package com.hoangphi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.minidev.json.annotate.JsonIgnore;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="pet_id")
+    @JsonIgnore
+    private Pet pet;
+}

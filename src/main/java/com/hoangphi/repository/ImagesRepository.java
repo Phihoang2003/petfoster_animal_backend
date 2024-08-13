5783008f034d89b620a726795f0b7059b13389b6
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-13 11:15:31 +0700

    add getImages by productId controller and repository

--- src/main/java/com/hoangphi/repository/ImagesRepository.java
+++ src/main/java/com/hoangphi/repository/ImagesRepository.java
@@ -2,8 +2,14 @@ package com.hoangphi.repository;
 
 import com.hoangphi.entity.Imgs;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ImagesRepository extends JpaRepository<Imgs, Integer> {
+    @Query(nativeQuery = true,value = "select * from imgs where product_id=:productId")
+    public List<Imgs> getImagesByProductId(@Param("productId") String productId);
 }

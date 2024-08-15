2cddcb518cc45b1b4bb44cfcd215a0c4030e38e0
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-15 14:38:30 +0700

    add filter pet repository and service

--- src/main/java/com/hoangphi/repository/PetRepository.java
+++ src/main/java/com/hoangphi/repository/PetRepository.java
@@ -15,4 +15,23 @@ public interface PetRepository extends JpaRepository<Pet, String>{
 
     @Query(nativeQuery = true,value = "select top 8 * from pet where (breed_id=:breedId or age=:size) and pet_id!=:petId")
     List<Pet> findByPetStyleAndIgnorePetId(@Param("breedId") String breedId, @Param("size") String size, @Param("petId") String petId);
+
+
+    @Query("SELECT p "+
+            "FROM Pet p "+
+            "INNER JOIN p.petBreed pb "+
+            "INNER JOIN pb.petType pt "+
+            "WHERE (:typeName IS NULL OR pt.name LIKE %:typeName%) "+
+            "AND (:name IS NULL OR p.petName like %:name%) "+
+            "AND (:colors IS NULL OR (CONCAT(',', :colors, ',') LIKE CONCAT('%,', p.petColor, ',%'))) "+
+            "AND (:age IS NULL OR p.age like %:age%) "+
+            "AND (:gender IS NULL OR p.sex = :gender) "+
+            "ORDER BY "+
+            "CASE WHEN :sort = 'oldest' THEN p.fosterAt END ASC, "+
+            "CASE WHEN :sort = 'latest' THEN p.fosterAt END DESC"
+    )
+    List<Pet> filterPets(@Param("name") String name, @Param("typeName") String typeName,
+                         @Param("colors") String colors,
+                         @Param("age") String age, @Param("gender") Boolean gender, @Param("sort") String sort);
+
 }
--- src/main/java/com/hoangphi/service/pets/PetService.java
+++ src/main/java/com/hoangphi/service/pets/PetService.java
@@ -1,12 +1,19 @@
 package com.hoangphi.service.pets;
 
+import com.hoangphi.entity.Pet;
+import com.hoangphi.entity.User;
 import com.hoangphi.request.pets.PetRequest;
 import com.hoangphi.response.ApiResponse;
+import com.hoangphi.response.pets.PetResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PetService {
+
+    List<PetResponse> buildPetResponse(List<Pet> petsRaw, User user);
+
     ApiResponse createPet(PetRequest petRequest, List<MultipartFile> images);
 
     ApiResponse updatePet(String id, PetRequest petRequest);
@@ -18,4 +25,11 @@ public interface PetService {
     ApiResponse getFavorites(String token, int page);
 
     ApiResponse getDetailPet(String id);
+
+    ApiResponse getPetManagement(String id);
+
+    ApiResponse deletePet(String id);
+
+    ApiResponse filterPets(Optional<String> name, Optional<String> typeName, Optional<String> colors, Optional<String> age,
+                           Optional<Boolean> gender, Optional<String> sort, Optional<Integer> page);
 }

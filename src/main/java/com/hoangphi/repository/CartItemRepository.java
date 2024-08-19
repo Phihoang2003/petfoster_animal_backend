bf1dc184d2063a3bc4d4fb39acba747e1a0c64f9
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-19 11:24:30 +0700

    add update cart

--- src/main/java/com/hoangphi/repository/CartItemRepository.java
+++ src/main/java/com/hoangphi/repository/CartItemRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
     @Query("select ci from CartItem ci where ci.productRepo.product.id=:productId and ci.productRepo.size=:size and ci.cart.cartId=:cartId")
     Optional<CartItem> findBySizeAndProductId(@Param("cartId") Integer cartId,@Param("productId") String productId,@Param("size") int size);
+
+    @Query(nativeQuery = true, value = "select * from cart_item where cart_id = :cartId")
+    List<CartItem> findByCartsId(@Param("cartId") Integer cartId);
 }
--- src/main/java/com/hoangphi/service/carts/CartService.java
+++ src/main/java/com/hoangphi/service/carts/CartService.java
@@ -3,6 +3,10 @@ package com.hoangphi.service.carts;
 import com.hoangphi.request.carts.CartRequest;
 import com.hoangphi.response.ApiResponse;
 
+import java.util.List;
+
 public interface CartService {
     ApiResponse createCart(String jwt, CartRequest cartRequest);
+
+    ApiResponse updateCarts(String jwt, List<CartRequest> cartRequests);
 }

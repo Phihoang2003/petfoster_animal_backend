210754c0b1dfb6b1fdb70a282909eb4867e1a251
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-20 13:19:51 +0700

    create delivery company

---
+++ src/main/java/com/hoangphi/entity/DeliveryCompany.java
@@ -0,0 +1,26 @@
+package com.hoangphi.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Nationalized;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class DeliveryCompany {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Nationalized
+    private String company;
+
+    @OneToMany(mappedBy = "deliveryCompany", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<ShippingInfo> shippingInfos;
+}

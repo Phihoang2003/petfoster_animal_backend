b44ef6698823760d49898d6ad5062bdafac2db0b
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-20 22:09:28 +0700

    create getProvinceId

---
+++ src/main/java/com/hoangphi/controller/order/OrderController.java
@@ -0,0 +1,22 @@
+package com.hoangphi.controller.order;
+
+import com.hoangphi.request.order.OrderRequest;
+import com.hoangphi.response.ApiResponse;
+import com.hoangphi.service.order.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping("order")
+    public ResponseEntity<ApiResponse> order(@RequestHeader("Authorization") String jwt,
+                                             @Valid @RequestBody OrderRequest orderRequest) {
+        return ResponseEntity.ok(orderService.order(jwt, orderRequest));
+    }
+}

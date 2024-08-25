b7a83d60e507fb24453b666a06753b5377cacbc9
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-25 17:59:09 +0700

    create product item

---
+++ src/main/java/com/hoangphi/response/orders_history/OrderProductItem.java
@@ -0,0 +1,22 @@
+package com.hoangphi.response.orders_history;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderProductItem {
+    private String id;
+    private String brand;
+    private String image;
+    private String name;
+    private Object size;
+    private Integer price;
+    private Integer quantity;
+    private Integer repo;
+    private Boolean isRate;
+}

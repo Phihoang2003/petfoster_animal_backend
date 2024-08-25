4a14c698ecf1e59fca2b24d62862ae5455a14273
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-25 18:39:42 +0700

    fix bug for duplicate [id] in order history

--- src/main/java/com/hoangphi/repository/OrderRepository.java
+++ src/main/java/com/hoangphi/repository/OrderRepository.java
@@ -2,8 +2,18 @@ package com.hoangphi.repository;
 
 import com.hoangphi.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Orders, Integer> {
+    @Query(nativeQuery = true, value = "select o.*,o.shipping_info_id as orderShipId from orders o  " +
+            "inner join shipping_info si on si.id = o.shipping_info_id " +
+            "where [user_id] = :userId and o.[status] like %:status% " +
+            "order by " +
+            "create_at desc")
+    public List<Orders> orderHistory(@Param("userId") String userId, @Param("status") String status);
 }

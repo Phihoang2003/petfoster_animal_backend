6248934b8f0c9554d36444a3214c8a25117fcb2e
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-26 17:01:47 +0700

    done get and update search history

---
+++ src/main/java/com/hoangphi/controller/search_history/SearchHistoryController.java
@@ -0,0 +1,24 @@
+package com.hoangphi.controller.search_history;
+
+import com.hoangphi.response.ApiResponse;
+import com.hoangphi.service.search_history.SearchHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user/search-histories")
+@RequiredArgsConstructor
+public class SearchHistoryController {
+    private final SearchHistoryService searchHistoryService;
+    @GetMapping("")
+    public ResponseEntity<ApiResponse> getSearchHistopry(@RequestHeader("Authorization") String jwt) {
+        return ResponseEntity.ok(searchHistoryService.getSearchHistory(jwt));
+    }
+
+    @PutMapping("")
+    public ResponseEntity<ApiResponse> updateSearchHistopry(@RequestHeader("Authorization") String jwt,
+                                                           @RequestParam String keyword) {
+        return ResponseEntity.ok(searchHistoryService.updateSearchHistory(jwt, keyword));
+    }
+}
---
+++ src/main/java/com/hoangphi/repository/SearchHistoryRepository.java
@@ -0,0 +1,16 @@
+package com.hoangphi.repository;
+
+import com.hoangphi.entity.SearchHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SearchHistoryRepository extends JpaRepository<SearchHistory,Integer> {
+    @Query("SELECT s FROM SearchHistory s WHERE s.user.id = :userId ORDER BY s.searchAt DESC")
+    public Optional<List<SearchHistory>> findByUserId(@Param("userId") String userId);
+}

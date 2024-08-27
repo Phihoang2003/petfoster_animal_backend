f17e3ce6f051aea9fe9c608ebd477417077b80d7
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-27 09:48:07 +0700

    create findSizeByProduct and min price

--- src/main/java/com/hoangphi/repository/ProductRepoRepository.java
+++ src/main/java/com/hoangphi/repository/ProductRepoRepository.java
@@ -18,4 +18,10 @@ public interface ProductRepoRepository extends JpaRepository<ProductRepo, Intege
 
     @Query(nativeQuery = true, value = "select * from product_repo where product_id = :productId and size = :size")
     public ProductRepo findProductRepoByIdAndSize(@Param("productId") String productId, @Param("size") Integer size);
+
+    @Query("select rp.size from ProductRepo rp where rp.product.id = :productId order by rp.size")
+    List<Integer> findSizeByProduct(@Param("productId") String productId);
+
+    @Query(nativeQuery = true, value = "select top 1 * from product_repo where product_id = :productId order by out_price")
+    ProductRepo findByProductMinPrice(@Param("productId") String productId);
 }

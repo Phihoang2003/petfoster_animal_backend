b9b3938897fb079fe14b59e925cd0015219eede4
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-09-01 22:34:15 +0700

    resolve conflict

--- src/main/java/com/hoangphi/constant/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.hoangphi.constant;
-
-public class Constant {
-    public static final String BASE_URL="http://localhost:8019/login/";
-    public static final String SECRET_KEY = "dfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhfdfhdsfjhjdfjsdhf";
-    public static final Integer TOKEN_EXPIRE_LIMIT = 5 * 60 * 1000;
-}

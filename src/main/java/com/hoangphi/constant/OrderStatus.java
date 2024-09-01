350dc7fa3d1b8db13878db30662aac306b768d3c
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-09-01 16:11:51 +0700

    remove constant

---
+++ src/main/java/com/hoangphi/constant/OrderStatus.java
@@ -0,0 +1,18 @@
+package com.hoangphi.constant;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum OrderStatus {
+    PLACED("Placed"),
+    SHIPPING("Shipping"),
+    DELIVERED("Delivered"),
+    CANCELLED_BY_CUSTOMER("Cancelled By Customer"),
+    CANCELLED_BY_ADMIN("Cancelled By Admin"),
+    WAITING("Waiting");
+
+    private final String state;
+    public String getValue() {
+        return state;
+    }
+}
